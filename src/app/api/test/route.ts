6081eaa77f22a602efc62e46f429486d09eb02be
Axel Catois - CodingBill <contact@codingbill.com>
2023-09-29 14:42:52 +0200

    remove fs

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,11 +3,5 @@ import fs from "fs";
 const path = "./public/data";
 
 export async function GET(request: Request) {
-  if (!fs.existsSync(path)) {
-    console.log("WRITING FILE !", __dirname);
-    fs.mkdirSync(path);
-    fs.writeFileSync("./public/data/test.txt", "coucou");
-  }
-
   return new Response(JSON.stringify("coucou"));
 }
