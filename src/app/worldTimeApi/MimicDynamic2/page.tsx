7b56a67fb42b6e59be64e7465d025fc330cdc68c
Axel Catois - CodingBill <contact@codingbill.com>
2023-10-09 11:42:06 +0200

    component name

--- src/app/worldTimeApi/MimicDynamic2/page.tsx
+++ src/app/worldTimeApi/MimicDynamic2/page.tsx
@@ -1,6 +1,6 @@
 import getValue from "@/helpers/getValue";
 
-export default async function Tags() {
+export default async function MimicDynamic2() {
   /* Visit this page, wait 30 sec then click the link to this page again to see changes (it becomes dynamic for 5 min straight) */
 
   const data = await fetch("https://worldtimeapi.org/api/timezone/Europe/Andorra", {
